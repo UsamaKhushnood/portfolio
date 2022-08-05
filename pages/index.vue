4864929a774d1627b08361c44e142f5c34f3364c
UsamaKhushnood <44152071+UsamaKhushnood@users.noreply.github.com>
2022-08-05 22:40:06 +0500

    no slider

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <HeroSec />
-    <WorkSec />
+    <!-- <WorkSec /> -->
     <SkillsSec />
-    <TestimonialsSec />
+    <!-- <TestimonialsSec /> -->
     <ContactSec />
   </div>
 </template>
